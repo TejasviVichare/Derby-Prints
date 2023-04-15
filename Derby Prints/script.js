b3fb0a59e34ec20f3508a05b2040f4b10af6b52b
TejasviVichare <125802591+TejasviVichare@users.noreply.github.com>
2023-04-15 13:05:36 +0530

    Add files via upload

---
+++ Derby Prints/script.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  sliderPerGroup: 3,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+ 
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    600: {
+      slidesPerView: 2,
+    },
+    1001: {
+      slidesPerView: 3,
+    },
+   
+  },
+});
+ 
